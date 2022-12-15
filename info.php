ec45469894084661d808e141748202212f686bff
Nurbek Imangazin <39064313+Imangazin@users.noreply.github.com>
2022-12-15 14:34:46 -0500

    Add files via upload

---
+++ info.php
@@ -0,0 +1,27 @@
+<?php
+//Quick Add LTI info.php
+
+//require_once 'ims-blti/blti.php';
+
+//The LTI Credentials for index.php
+
+//You need to set the LTI Key and LTI Secret
+//$lti_auth = array('key' => '', 'secret' => '');
+
+//Support Email shown to users in the tool
+//$supportEmail = "";
+
+$config = array(
+    'libpath'    => 'lib',
+
+    'host'       => '',
+    'port'       => 443,
+    'scheme'     => 'https',
+
+    'appId'      => '',
+    'appKey'     => '',
+    'userId'     => '',
+    'userKey'    => '',
+
+    'LP_Version' => '1.36');
+?>
